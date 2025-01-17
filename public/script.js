bf889f977a375dc2e2722faa937f0a693840a414
th3ev3 <andreymacedo21211@gmail.com>
2025-01-17 14:32:43 -0300

    remoção de comentarios desnecessarios

--- public/script.js
+++ public/script.js
@@ -46,9 +46,7 @@ window.addEventListener("click", () => {
 
 window.addEventListener('load', function () {
     var audio = document.getElementById('sambaSong');
-    // Tenta iniciar o áudio sem interação
     audio.play().catch(function () {
-        // Em caso de bloqueio, tenta após uma interação do usuário
         document.addEventListener('click', function () {
             audio.play();
         }, {
@@ -57,61 +55,55 @@ window.addEventListener('load', function () {
     });
 });
 
-// Função para exibir o alerta personalizado
 function showCustomAlert(message) {
     document.querySelector('.custom-alert-content p').innerText = message;
     document.getElementById('custom-alert').style.display = 'block';
 }
 
-// Função para fechar o alerta personalizado
 function closeCustomAlert() {
     document.getElementById('custom-alert').style.display = 'none';
 }
 
-// Bloquear o menu de contexto (clique direito)
 document.addEventListener('contextmenu', function (e) {
     e.preventDefault();
     showCustomAlert("Proibido tentar salvar qualquer conteúdo");
 });
 
-// Bloquear atalhos de teclado para salvar, copiar, imprimir ou ver código-fonte
 document.addEventListener('keydown', function (e) {
     if (e.ctrlKey || e.metaKey) {
         if (e.key === 's' || e.key === 'p' || e.key === 'c' || e.key === 'u') {
-            e.preventDefault(); // Bloquear Ctrl+S, Ctrl+P, Ctrl+C, Ctrl+U
+            e.preventDefault(); 
             showCustomAlert("Esta ação foi desativada.");
         }
     }
 });
 
-// Bloquear a função de arrastar imagens (para impedir arrastar e salvar)
 document.addEventListener('dragstart', function (e) {
     e.preventDefault();
     showCustomAlert("Arrastar e salvar conteúdo foi desativado.");
 });
 
-// Função para abrir um card específico
 function openCard(cardId) {
     document.getElementById('overlay').style.display = 'block';
     document.getElementById(cardId).classList.add('active');
 }
 
-// Função para fechar todos os cards
+
 function closeCard() {
     document.getElementById('overlay').style.display = 'none';
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => card.classList.remove('active'));
 }
 
-// Função para abrir outro card a partir de um card atual
+
 function openOtherCard(cardId) {
     closeCard();
-    setTimeout(() => { // Pequeno delay para transição suave
+    setTimeout(() => { 
         openCard(cardId);
     }, 300);
 }
 
-// Função para visualizar o resumo do comunicado com a imagem
+
 function verComunicado(comunicadoId, imgSrc) {
     const comunicados = {
         'comunicado1': '<br>Pois é! Esse é um benefício que estendemos a todos os colaboradores e também à família de cada um de vocês! <br><br>Aproveitem essa oportunidade de ouro para acessar uma série de serviços, como lazer, esporte, saúde, cultura e muito mais.',
@@ -142,22 +134,21 @@ function verComunicado(comunicadoId, imgSrc) {
 
     const resumo = comunicados[comunicadoId] || 'Resumo não disponível no momento.';
 
-    // Exibe o resumo do comunicado com HTML (para permitir links clicáveis)
+
     document.getElementById('resumo-conteudo').innerHTML = resumo;
 
-    // Verifica se imgSrc é válido e exibe a imagem
     const comunicadoImagem = document.getElementById('comunicado-imagem');
     if (imgSrc) {
         comunicadoImagem.src = imgSrc;
-        comunicadoImagem.style.display = 'block'; // Exibe a imagem
+        comunicadoImagem.style.display = 'block'; 
     } else {
-        comunicadoImagem.style.display = 'none'; // Oculta a imagem se não houver
+        comunicadoImagem.style.display = 'none';
     }
 
     openCard('resumo-card');
 }
 
-// Função para fechar o card de resumo do comunicado
+
 function closeResumoCard() {
     document.getElementById('resumo-card').classList.remove('active');
     document.getElementById('overlay').style.display = 'none';
@@ -166,31 +157,24 @@ function closeResumoCard() {
 const popup = document.getElementById('popup');
 const closeButton = document.getElementById('close-button');
 
-// Função para abrir o popup
 function showPopup() {
     document.getElementById('popup-novo-comunicado').style.display = 'block';
 }
 
-// Função para fechar o popup
 function closePopup() {
     document.getElementById('popup-novo-comunicado').style.display = 'none';
 }
 
-// Função para redirecionar ao comunicado
 function goToComunicado() {
     closePopup(); // Fechar o popup
-    window.location.href = '#comunicado'; // Alterar para o link ou âncora correta
+    window.location.href = '#comunicado'; 
 }
 
-// Exibir o popup após um pequeno atraso (ex: 3 segundos)
-setTimeout(showPopup, 5); // 3000 milissegundos = 3 segundos
+setTimeout(showPopup, 5); 
 
-// Verifica se o navegador suporta notificações
 if ("Notification" in window) {
-    // Solicita permissão assim que o site é carregado
     window.addEventListener('load', () => {
-        // Verifica se a permissão ainda não foi definida pelo usuário
-        if (Notification.permission === "default") { // "default" significa que o usuário ainda não escolheu
+        if (Notification.permission === "default") { 
             Notification.requestPermission();
         }
     });
